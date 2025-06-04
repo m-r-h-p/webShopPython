a345e2af1d568555101e0801e2924ac6f63bf828
AmirHp <amir.hpid@gmail.com>
2025-06-04 15:47:35 +0330

    crate register page view url

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('accounts/register/', views.register, name='register'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def home(request):
     products = Product.objects.filter(available=True)
@@ -10,3 +12,17 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, available=True)
     comments = product.comments.filter(approved=True)
     return render(request, 'shop/product_detail.html', {'product': product, 'comments': comments})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #user = form.save()
+           # login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
