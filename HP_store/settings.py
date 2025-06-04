e43da2e7dbda54ecd72776ac0866e044be1b8422
AmirHp <amir.hpid@gmail.com>
2025-06-04 15:36:44 +0330

    crate loggin page

--- HP_store/settings.py
+++ HP_store/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'HP_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,9 +64,6 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-    },
-    {
-        'DIRS': [BASE_DIR / 'templates'],
     },
 ]
 
