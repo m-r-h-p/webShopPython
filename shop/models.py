141fb67374b853a5a5cdf84d857d0a3f0e31b942
AmirHp <amir.hpid@gmail.com>
2025-06-04 16:33:22 +0330

    add cart models

--- shop/models.py
+++ shop/models.py
@@ -32,3 +32,18 @@ class Comment(models.Model):
     approved =models.BooleanField(default=False)
     def __str__(self):
         return f'Comment by {self.user.username} on {self.product.name}'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Cart for {self.user.username}'
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return f'{self.quantity} of {self.product.name}'
